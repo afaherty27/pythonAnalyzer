fcce53e48233bf7382437819bc6a58cae7b353f3
Adam <afaherty27@gmail.com>
2016-01-09 02:40:21 -0600

    fixed bug displaying incorrect list length

--- summaryReport.py
+++ summaryReport.py
@@ -1,11 +1,35 @@
+import os
+import datetime
 
-#function to count words
-def wordCount(file) :
+#This module counts how many words are in the file that is analyzed
 
-	#process words in list
-	return count
-	
+tokenCount = []
+wordCount = 0
 
-#def writeOutputFile() :
-	
+# process list of words to get a count of how many are in the list
+def summaryProcessTokens(words) :
 
+	global wordCount
+	wordCount = len(words)
+
+# write output file containing summary of file analysis        
+def summaryWriteOutputFile(textFile) :
+
+	statement = 'Application: Python Analyzer\n'
+	author = 'Author: Adam Faherty\n'
+	analyzedFile = 'Input File: ' + str(textFile.name) + '\n'
+	currentTime = 'Analyed on: ' + str(datetime.datetime.now()) + '\n'
+	result = 'Word Count: '+ str(wordCount)
+
+	filePath = 'C:/Users/L/Dropbox/enterprisejava/pythonAnalyzer/summaryReport.txt'
+	mode = 'a' if os.path.exists(filePath) else 'w+'
+
+	with open(filePath, mode) as file :
+
+		file.seek(0)
+		file.truncate() #clears file if it exists already
+		file.write(statement)
+		file.write(author)
+		file.write(analyzedFile)
+		file.write(currentTime)
+		file.write(result)
