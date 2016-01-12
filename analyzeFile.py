313b96dfb2ca13847d0132ad3d6bc3d9e1b4ffb6
Adam Faherty <afaherty27@gmail.com>
2016-01-11 20:29:48 -0600

    process unique tokens added

--- analyzeFile.py
+++ analyzeFile.py
@@ -1,12 +1,15 @@
 __author__ = 'afaherty'
 from summaryReport import summary_process_tokens
 from summaryReport import summary_write_output_file
+from uniqueTokenAnalyzer import  unique_process_tokens
 
-
+#passes list of words to each analyzer for processing
 def process_tokens(text_file) :
     summary_process_tokens(words)
+    unique_process_tokens(words)
 
 
+#writes output file of each analyzer
 def write_output_files(text_file) :
     summary_write_output_file(text_file)
 
@@ -19,7 +22,6 @@ file_name = input()
 with open(file_name, 'r') as text_file :
 
     words = text_file.read().split()
-    #summary_process_tokens(words)
 
     process_tokens(text_file)
     write_output_files(text_file)
