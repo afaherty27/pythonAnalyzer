951149055ec6e511c69b9d0725601a506c27ab9c
Adam <afaherty27@gmail.com>
2016-01-13 02:13:15 -0600

    fixed issues with parameters

--- analyzeFile.py
+++ analyzeFile.py
@@ -1,18 +1,18 @@
 __author__ = 'afaherty'
 from summaryReport import summary_process_tokens
 from summaryReport import summary_write_output_file
-from uniqueTokenAnalyzer import  unique_process_tokens
+from uniqueTokenAnalyzer import unique_process_tokens
+from uniqueTokenAnalyzer import unique_token_output_file
 
-#passes list of words to each analyzer for processing
-def process_tokens(text_file) :
+
+def process_tokens(words) :
     summary_process_tokens(words)
     unique_process_tokens(words)
 
 
-#writes output file of each analyzer
 def write_output_files(text_file) :
     summary_write_output_file(text_file)
-
+    unique_token_output_file(text_file)
 
 # input to hold file
 print('enter file to read:')
@@ -22,6 +22,8 @@ file_name = input()
 with open(file_name, 'r') as text_file :
 
     words = text_file.read().split()
-
-    process_tokens(text_file)
+    #works for small file. not large
+    process_tokens(words)
     write_output_files(text_file)
+
+    
