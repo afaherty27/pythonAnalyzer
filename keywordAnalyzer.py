7ffb4115027f541695bd12453a5d40bdd6c8b5d4
Adam Faherty <afaherty27@gmail.com>
2016-02-22 23:54:06 -0600

    prints list of indexes related to keyword

--- keywordAnalyzer.py
+++ keywordAnalyzer.py
@@ -1,11 +1,27 @@
-__author__ = 'L'
+__author__ = 'afaherty'
 
 import os
 
-
+# method compares words in keyword file to words in analysis file
 def keyword_process_tokens(words) :
-    print('hello keyword')
 
+    with open('keywords.txt', 'r') as keyword_file :
+
+        keywords = keyword_file.read().split()
+
+        for keyword in keywords:
+
+            index_list = []
+            for index, word in enumerate(words) :
+
+                if word.lower() == keyword :
+                    index_list.append(index)
+
+            write_keyword_data(keyword, index_list)
+
+
+def write_keyword_data(keyword, index_list) :
+    print(keyword + str(index_list))
 
 def keyword_output_file(text_file) :
     print('hello keyword output')
